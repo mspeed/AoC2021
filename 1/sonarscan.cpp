40cd0889ee17d3dcfce5208e8ed79f0d0f96b6c9
mspeed <mspeed@users.noreply.github.com>
2021-12-02 23:18:57 +0000

    It's an SMA :) Ugly prefill though.

--- 1/sonarscan.cpp
+++ 1/sonarscan.cpp
@@ -6,17 +6,32 @@ using std::string;
 using std::cout; using std::endl;
 using std::fstream;
 
+int32_t ThreeTapSMA(int32_t const& Input)
+{
+  static int32_t x1 = 0;
+  static int32_t x2 = 0;
+
+  int32_t const Output = Input + x1 + x2;
+
+  x2 = x1; x1 = Input;
+  //return (x1 && x2 ) ? Output : -1;
+  return Output;
+}
+
 fstream& ParseDepths(fstream& In, int32_t& Count)
 {
   int32_t Depth;
   int32_t PreviousDepth = -1;
+  int32_t PreFill = -2;
   while(In >> Depth)
   {
+    Depth = ThreeTapSMA(Depth);
+    if(PreFill < 0){ PreFill++; continue; }
     if(Depth > PreviousDepth) { Count++; }
     PreviousDepth = Depth;
   }
+  return In;
 }
-
 
 int main(int argc, char * argv[])
 {
